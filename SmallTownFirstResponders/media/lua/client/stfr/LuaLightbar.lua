0287bdc5ca2e02264941e344c1dff578636db1ea
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2024-08-03 09:19:41 +0100

    add support for per-skin siren sounds

--- SmallTownFirstResponders/media/lua/client/stfr/LuaLightbar.lua
+++ SmallTownFirstResponders/media/lua/client/stfr/LuaLightbar.lua
@@ -14,7 +14,7 @@ local currentTime = 0
 ---@field private _lastX integer
 ---@field private _lastY integer
 ---@field private _lastZ integer
----@field private _lastMode LightbarMode
+---@field private _lastLightMode LightbarLightMode
 ---@field private _lastSide LightbarActiveLight
 ---@field private _leftR number
 ---@field private _leftG number
@@ -22,6 +22,12 @@ local currentTime = 0
 ---@field private _rightR number
 ---@field private _rightG number
 ---@field private _rightB number
+---@field private _emitter BaseSoundEmitter
+---@field private _sirenSound number
+---@field private _lastSirenMode LightbarSirenMode
+---@field sirenYelp string?
+---@field sirenWail string?
+---@field sirenAlarm string?
 local LuaLightbar = {}
 LuaLightbar.__index = LuaLightbar
 ---@type LuaLightbar[]
@@ -32,14 +38,37 @@ LuaLightbar.list = {}
 ---| 1 # Left
 ---| 2 # Right
 
----@alias LightbarMode
+---@alias LightbarLightMode
 ---| 0 # Off
 ---| 1 # Slow blink
 ---| 2 # Double blink
 ---| 3 # Fast blink
 
+---@alias LightbarSirenMode
+---| 0 # Off
+---| 1 # Yelp
+---| 2 # Wail
+---| 3 # Alarm
+
 ---@private
----@param mode LightbarMode
+---@type Callback_OnEnterVehicle|Callback_OnExitVehicle
+LuaLightbar._handleEnterExitVehicle = function(character)
+    if not character:isLocal() then return end
+    local vehicle = character:getVehicle()
+    for i = 1, #LuaLightbar.list, 1 do
+        local lightbar = LuaLightbar.list[i]
+        if lightbar._vehicle == vehicle then
+            if lightbar._sirenSound == -1 then return end
+            lightbar._emitter:set3D(lightbar._sirenSound, vehicle:isAnyListenerInside())
+        end
+    end
+end
+
+Events.OnEnterVehicle.Add(LuaLightbar._handleEnterExitVehicle)
+Events.OnExitVehicle.Add(LuaLightbar._handleEnterExitVehicle)
+
+---@private
+---@param mode LightbarLightMode
 ---@return LightbarActiveLight
 LuaLightbar._getActiveLight = function(self, mode)
     local time = currentTime - self._startTime
@@ -72,6 +101,18 @@ LuaLightbar._getActiveLight = function(self, mode)
     return 0
 end
 
+---@param mode LightbarLightMode
+---@return string?
+LuaLightbar._getSirenSound = function(self, mode)
+    if mode == 1 then
+        return self.sirenYelp
+    elseif mode == 2 then
+        return self.sirenWail
+    elseif mode == 3 then
+        return self.sirenAlarm
+    end
+end
+
 ---@param r number
 ---@param g number
 ---@param b number
@@ -95,6 +136,15 @@ LuaLightbar.getVehicle = function(self)
     return self._vehicle
 end
 
+---@param yelp string
+---@param wail string
+---@param alarm string
+LuaLightbar.setSirenSounds = function(self, yelp, wail, alarm)
+    self.sirenYelp = yelp
+    self.sirenWail = wail
+    self.sirenAlarm = alarm
+end
+
 local vec3 = Vector3f.new()
 
 ---Updates the position and colour of the active light
@@ -104,26 +154,46 @@ LuaLightbar.update = function(self)
         return
     end
 
-    ---@type LightbarMode
-    local mode = self._vehicle:getLightbarLightsMode()
+    ---@type LightbarLightMode
+    local lightMode
+    ---@type LightbarSirenMode
+    local sirenMode
 
     if self._vehicle:getBatteryCharge() <= 0 then
-        mode = 0
+        lightMode = 0
+        sirenMode = 0
+    else
+        lightMode = self._vehicle:getLightbarLightsMode()
+        sirenMode = self._vehicle:getLightbarSirenMode()
     end
 
-    if mode ~= self._lastMode then
-        self._lastMode = mode
+    if sirenMode ~= self._lastSirenMode then
+        if self._sirenSound ~= -1 then
+            self._emitter:stopSoundLocal(self._sirenSound)
+        end
+        local sound = self:_getSirenSound(sirenMode)
+        if sound then
+            self._sirenSound = self._emitter:playSoundLoopedImpl(sound)
+        else
+            self._sirenSound = -1
+        end
+        self._lastSirenMode = sirenMode
+    end
+
+    if lightMode ~= self._lastLightMode then
+        self._lastLightMode = lightMode
         self._startTime = getTimeInMillis()
-        if mode == 0 then
+
+        if lightMode == 0 then
             cell:removeLamppost(self._light)
             self._lastSide = 0
             return
         end
-    elseif mode == 0 then
+    elseif lightMode == 0 then
         return
     end
 
-    local activeSide = self:_getActiveLight(mode)
+    local activeSide = self:_getActiveLight(lightMode)
 
     if activeSide ~= 0 then
         local x,y,z = 0,0,0
@@ -172,12 +242,12 @@ end
 
 ---Creates a LuaLightbar for a vehicle with the given colours
 ---@param vehicle BaseVehicle The vehicle
----@param leftR number Left red
----@param leftG number Left green
----@param leftB number Left blue
----@param rightR number Right red
----@param rightG number Right green
----@param rightB number Right blue
+---@param leftR? number Left red
+---@param leftG? number Left green
+---@param leftB? number Left blue
+---@param rightR? number Right red
+---@param rightG? number Right green
+---@param rightB? number Right blue
 ---@return LuaLightbar
 LuaLightbar.new = function(vehicle, leftR, leftG, leftB, rightR, rightG, rightB)
     local o = {}
@@ -186,20 +256,21 @@ LuaLightbar.new = function(vehicle, leftR, leftG, leftB, rightR, rightG, rightB)
     ---@cast o LuaLightbar
 
     o._vehicle = vehicle
+    o._emitter = vehicle:getEmitter()
+    o._leftR = leftR or 0
+    o._leftG = leftG or 0
+    o._leftB = leftB or 0
+    o._rightR = rightR or 0
+    o._rightG = rightG or 0
+    o._rightB = rightB or 0
+
     o._startTime = -1
-
-    o._leftR = leftR
-    o._leftG = leftG
-    o._leftB = leftB
-    o._rightR = rightR
-    o._rightG = rightG
-    o._rightB = rightB
-
     o._lastX = 0
     o._lastY = 0
     o._lastZ = 0
-    o._lastMode = 0
+    o._lastLightMode = 0
     o._lastSide = 0
+    o._sirenSound = -1
 
     -- this one gets deleted right away but needs to exist to avoid a null pointer error
     o._light = IsoLightSource.new(0, 0, 0, 0, 0, 0, 8)
@@ -231,12 +302,23 @@ end)
 ---Creates a LuaLightbar for a vehicle
 ---@type VehiclePart_init
 LuaLightbar_Init = function(vehicle, part)
+    local lightbar = LuaLightbar.new(vehicle)
+
+    local skinIndex = vehicle:getSkinIndex()
     local colours = part:getTable("colours")
-    local skinColours = luautils.split(colours[vehicle:getSkinIndex()] or colours.default, ";")
-    for i = 1, 6 do
-        skinColours[i] = tonumber(skinColours[i])
+    if colours then
+        local skinColours = luautils.split(colours[skinIndex] or colours.default, ";")
+        for i = 1, 6 do
+            skinColours[i] = tonumber(skinColours[i])
+        end
+        lightbar:setColourLeft(skinColours[1], skinColours[2], skinColours[3])
+        lightbar:setColourRight(skinColours[4], skinColours[5], skinColours[6])
     end
-    LuaLightbar.new(vehicle, unpack(skinColours))
+
+    local sirens = part:getTable("sirens")
+    if sirens then
+        lightbar:setSirenSounds(unpack(luautils.split(sirens[skinIndex] or sirens.default, ";")))
+    end
 end
 
 return LuaLightbar
